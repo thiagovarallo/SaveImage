114a50aa23033001adef2f28a2ec265313b96057
Thiago Varallo <thiagovaralllo@gmail.com>
2023-10-27 20:21:18 -0300

    New contrller and Action Method for Image Upload

---
+++ SaveImage]/Controllers/ImagesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SaveImage_.DATA.DTOs;
+
+namespace SaveImage_.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ImagesController : ControllerBase
+    {
+        [HttpPost("Upload")]
+        public async Task<IActionResult> Upload([FromForm] FileUploadRequestDto request)
+        {
+            ValidateFileUpload(request);
+
+            if (ModelState.IsValid)
+            {
+                //User repository to upload image
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        private void ValidateFileUpload(FileUploadRequestDto request)
+        {
+            var allowedEctensions = new string[] { ".jpg", ".jpeg", ".png" };
+
+            if (!allowedEctensions.Contains(Path.GetExtension(request.File.FileName)))
+            {
+                ModelState.AddModelError("file", "Unsepported file extension");
+            }
+
+            if (request.File.Length > 10485760)
+            {
+                ModelState.AddModelError("file", "File size more than 10MB, please upload a smaller size file.");
+            }
+        }
+    }
+}
---
+++ SaveImage]/DATA/DTOs/FileUploadRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SaveImage_.DATA.DTOs
+{
+    public class FileUploadRequestDto
+    {
+        public IFormFile File { get; set; }
+        public string FileName { get; set; }
+        public string FileDescription { get; set; }
+    }
+}
